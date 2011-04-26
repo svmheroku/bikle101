19362c80cfaa426d97e7d317606bea7dc0ab9251
AudreyLeeMe <audreyleemee@yahoo.com>
2011-04-26 19:36:47 +0100

    c1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikle101::Application.config.secret_token = '31c2d4de6992f0e341a444304bdf3e6158d215d6f34c084fe6122dc285414c24a5cba24ff15b26d6d104c9ed3a41cd007b4842ed975afe4a5b6d5fb849462430'
